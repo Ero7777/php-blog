9644a0dc8ee42141d153bc104898a31d795d3399
test <student@increment.am>
2019-06-22 19:57:28 +0400

    Changes have been made

--- app/index.php
+++ app/index.php
@@ -5,14 +5,14 @@ if(!empty($_GET['page'])){
     $page=$_GET["page"];
 }
 
-require "header.php";
-if ($page=''){
+require "templates\header.php";
+if ($page==''){
     require "templates\index.php";
 }else{
 
 }
-require "aside.php";
-require "footer.php";
+require "templates\aside.php";
+require "templates\footer.php";
 
 ?>
 
